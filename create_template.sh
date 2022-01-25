dbc29b1150f284f765e6cb7a698d545df5881baa
dilandehonbra <85010706+dilandehonbra@users.noreply.github.com>
2022-01-25 10:10:45 -0300

    Automatizar criação dos itens.

---
+++ create_template.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+#Lista de CommandLine para criação do array
+awk '
+	function tratamento(si) {
+		gsub (/"CommandLine":/, "", si)
+		gsub(/^[[:space:]]+/, "", si)
+		gsub(/\x27/, "\\""\x27", si)
+		gsub(/^\x22+/, "", si)
+		gsub(/,$/, "", si)
+		gsub(/[[:space:]]+$/, "", si)
+		gsub(/\x22$/, "", si)
+		gsub(/[[:space:]]+$/, "", si)
+		gsub(/\x22$/, "", si)
+		gsub(/^\\+/, "", si)
+		gsub(/^\x22+/, "", si)
+		gsub(/\\$/, "%", si)
+		gsub(/(\\\x22)+/, "%", si)
+	return si
+}
+	/.*CommandLine.*/ {
+	print tratamento($0)
+}
+' $1 | awk '{print "\"""%"$0"%""\""}' >> result.manipula
+
+#########################################################
+ARRAY=(`cat result.manipula | tr " " "#"`)
+ARRAYS=(`cat result.manipula | tr " " "#" | sed 's/^\"//' | sed 's/\"$//'`)
+#Ip do host para criação das chaves
+IPHOST=$2
+
+#Porta de comunicação entre host/zabbix
+PORT=$3
+
+#Nome do Template, por padrão será agrupado ao grupo "Templates"
+TEMPLATE_NAME=$4
+
+
+
+        cat << EOF > arq.txt
+<?xml version="1.0" encoding="UTF-8"?>
+<zabbix_export>
+    <version>5.0</version>
+    <date>2022-01-18T21:25:29Z</date>
+    <groups>
+        <group>
+            <name>Templates</name>
+        </group>
+    </groups>
+    <templates>
+        <template>
+            <template>$TEMPLATE_NAME</template>
+            <name>$TEMPLATE_NAME</name>
+            <groups>
+                <group>
+                    <name>Templates</name>
+                </group>
+            </groups>
+            <applications>
+                <application>
+                    <name>GetPID</name>
+                </application>
+                <application>
+                    <name>NumberOfLogicalProcessor</name>
+                </application>
+                <application>
+                    <name>Processos Windows</name>
+                </application>
+                <application>
+                    <name>Proc param</name>
+                </application>
+            </applications>
+            <items>
+        <item>
+                    <name>NumberOfLogicalProcessor</name>
+                    <type>ZABBIX_ACTIVE</type>
+                    <key>system.cpu.num</key>
+                    <delay>1d</delay>
+                    <applications>
+                        <application>
+                            <name>NumberOfLogicalProcessor</name>
+                        </application>
+                    </applications>
+                </item>
+
+
+EOF
+####################CPU USAGE###############################
+for i in "${ARRAY[@]}"
+
+	do
+	echo "<item> <name>Get Process (\$3)</name> <type>EXTERNAL</type> <key>win.proc.param.cpu[${IPHOST},${PORT},${i}]</key> <applications> <application> <name>GetPID</name> </application> </applications> </item>" >> arq.txt
+
+done ;
+
+for l in "${ARRAYS[@]}"
+###TRATAMENTO PARA NOME DOS ITENS CALCULADOS###
+	do
+	 TRATAMENTO=$(echo $l | sed 's/[[:punct:]]/\./g' |tr " " .| sed -E 's/^[.]+//' | sed -E 's/[.]{1,}/\./g' | sed -E 's/[.]+$//')
+	 TRATAMENTONAME=$(echo $l | sed 's/^\%//'| sed 's/\%$//')
+
+	echo "<item> <name>CPU in % by Process (${TRATAMENTONAME})</name> <type>CALCULATED</type> <key>item.${TRATAMENTO}</key> <value_type>FLOAT</value_type> <units>%</units> <params>last(&quot;win.proc.param.cpu[${IPHOST},${PORT},\&quot;${l}\&quot;]&quot;)/last(&quot;system.cpu.num&quot;)</params> <applications> <application> <name>Proc param</name> </application> </applications> </item>" >> arq.txt
+done;
+
+####################MEMORY USAGE#############################
+
+for i in "${ARRAY[@]}"
+
+	do
+	echo "<item> <name>Get Process (\$3)</name> <type>EXTERNAL</type> <key>win.proc.param.mem[${IPHOST},${PORT},${i}]</key> <applications> <application> <name>GetPID</name> </application> </applications> </item>" >> arq.txt
+
+done ;
+
+for l in "${ARRAYS[@]}"
+###TRATAMENTO PARA NOME DOS ITENS CALCULADOS###
+	do
+	 TRATAMENTO=$(echo $l | sed 's/[[:punct:]]/\./g' |tr " " .| sed -E 's/^[.]+//' | sed -E 's/[.]{1,}/\./g' | sed -E 's/[.]+$//')
+	 TRATAMENTONAME=$(echo $l | sed 's/^\%//'| sed 's/\%$//')
+
+	echo "<item> <name>Memory usage in % by process (${TRATAMENTONAME})</name> <type>CALCULATED</type> <key>item.${TRATAMENTO}</key> <value_type>FLOAT</value_type> <units>%</units> <params>last(&quot;win.proc.param.mem[${IPHOST},${PORT},\&quot;${l}\&quot;]&quot;)/last(&quot;system.cpu.num&quot;)</params> <applications> <application> <name>Proc param</name> </application> </applications> </item>" >> arq.txt
+done;
+
+
+
+
+    cat << EOF >> arq.txt
+</items>
+            <macros>
+                <macro>
+                    <macro>{\$PROC_CPU_CRIT}</macro>
+                    <value>90</value>
+                    <description>Threshold de CPU pelo processo.</description>
+                </macro>
+            </macros>
+        </template>
+    </templates>
+</zabbix_export>
+
+EOF
+cat arq.txt | tr "#" " " >> templates.xml ; rm arq.txt
+root@cbmvbhmonit01:/usr/local/sbin/testes/TESTE/final# ^C
+root@cbmvbhmonit01:/usr/local/sbin/testes/TESTE/final# cat /lib/zabbix/externalscripts/win.proc.param.mem 
+IPHOST=$1
+PORTNUM=$2
+PARAM=$3
+
+
+GETPID="$(zabbix_get -s ${IPHOST} -p ${PORTNUM}  -k wmi.getall["root\cimv2,select * from Win32_Process where CommandLine like '${PARAM}' "] | jq | awk '/"ProcessId":/ {print $2}' | sed 's/,//g')" 
+
+zabbix_get -s ${IPHOST} -p ${PORTNUM} -k wmi.getall["root\cimv2,SELECT * FROM Win32_PerfFormattedData_PerfProc_Process where IDProcess='${GETPID}'"] | jq | awk '/WorkingSet.:/ {print $2}' | sed 's/[[:punct:]]//g'
